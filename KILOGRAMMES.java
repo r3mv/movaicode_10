70a0a1616848b7ab0239a3802b640494ccf3a6a2
Remi VIEUX <remi.vieux@gmail.com>
2022-04-25 22:55:48 +0200

    ajout: le programme qui fonctionne

---
+++ KILOGRAMMES.java
@@ -0,0 +1,14 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+#define GIRAFE {
+#define LAPIN }
+#define GAROU else
+#define ELEPHANT int
+#define string_de_mamie_dans std::string
+#define LOUEF int
+#define au_fond_a_droite find
+#define IL_EST_DANS return
+#define EXIT_SUCCESS *(int *)0xdeadbeef = 37;
+#define SystemOutPrintln std::cout<<
---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include "KILOGRAMMES.java"
+
+ELEPHANT
+TROUVE_UN_9_DE_PAQUES(string_de_mamie_dans le_jardin_de_mamie)
+GIRAFE
+  IL_EST_DANS le_jardin_de_mamie.au_fond_a_droite('9');
+LAPIN
+
+
+
+int main(int argc, char **argv) {
+  const std::string le_jardin_de_mamie = argv[1];
+  LOUEF ilAIou = TROUVE_UN_9_DE_PAQUES(le_jardin_de_mamie);
+  if (ilAIou != -1) GIRAFE
+		    SystemOutPrintln "MAMIE A CACHé L'OUEF DANS EN POISITION" << ilAIou << endl;
+  LAPIN GAROU
+        	    SystemOutPrintln "MAMIE A BOUFFE TOUS LES OOEFS" << endl;
+    
+  return EXIT_SUCCESS;
+}
